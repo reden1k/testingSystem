3dd66754d9044b6d6ce60a31fd5c8e7bfb2ba9ef
reden1k <dim23062006@gmail.com>
2024-02-20 12:38:20 +0300

    no function here yet (FUNCTIONS EMPTY)

---
+++ testsGenerator.js
@@ -0,0 +1,6 @@
+import readlineSync from 'readline-sync'; 
+
+export const makeTest = () => {
+    const input = readlineSync.question('How much question do you want? (limit 40)'); //maybe change limit
+    //some logic
+}
